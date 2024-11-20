a3f0c62b627ae5c3615d3cc96e827db387076ff1
= <=>
2024-11-20 21:09:18 +0300

    fork -> clone

--- solution/include/external_commands.h
+++ solution/include/external_commands.h
@@ -2,4 +2,12 @@
 #define EXTERNAL_COMMANDS_H
 
 bool handle_external_command(char* command, char* args);
+
+#define STACK_SIZE (1024 * 1024) // Размер стека для нового процесса
+
+struct clone_args {
+    char* command;
+    char* args;
+};
+
 #endif
--- solution/src/external_commands.c
+++ solution/src/external_commands.c
@@ -1,34 +1,54 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "external_commands.h"
+
+int child_function(void* arg) {
+    struct clone_args* clone_arg = (struct clone_args*)arg;
+    char* command = clone_arg->command;
+    char* args = clone_arg->args;
+
+    char* args_array[64];
+    int i = 0;
+    args_array[i++] = command;
+    char* token = strtok(args, " ");
+    while (token != NULL) {
+        args_array[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args_array[i] = NULL;
+
+    execvp(command, args_array);
+    perror("execvp");
+    return EXIT_FAILURE;
+}
 
 bool handle_external_command(char* command, char* args) {
-    pid_t pid;
-
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
+    void* child_stack = malloc(STACK_SIZE);
+    if (child_stack == NULL) {
+        perror("malloc");
         return false;
     }
 
-    if (pid == 0) {
-        char* args_array[64];
-        int i = 0;
-        args_array[i++] = command;
-        char* arg = strtok(args, " ");
-        while (arg != NULL) {
-            args_array[i++] = arg;
-            arg = strtok(NULL, " ");
-        }
-        args_array[i] = NULL;
-        execvp(command, args_array);
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        wait(NULL);
-        return true;
+    struct clone_args args_data = { command, args };
+    pid_t pid = clone(child_function, (char*) child_stack + STACK_SIZE, SIGCHLD, &args_data);
+    if (pid == -1) {
+        perror("clone");
+        free(child_stack);
+        return false;
     }
+
+    if (waitpid(pid, NULL, 0) == -1) {
+        perror("waitpid");
+        free(child_stack);
+        return false;
+    }
+
+    free(child_stack);
+    return true;
 }
