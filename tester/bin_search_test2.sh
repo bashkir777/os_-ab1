cce215ce02bebcc6954499f36158ce06395c5d55
= <=>
2024-11-21 09:23:33 +0300

    bench marks tests added

--- tester/bin_search_test2.sh
+++ tester/bin_search_test2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Assert binary search works correctly (target exists)"
+echo "Assert binary search works correctly (target does not exist)"
 
-expected_output="Target has been found. Position: 5"
-actual_output=$("../build/solution/lab1" bin-search 2 100 5 | grep -v "Execution time")
+expected_output="Target has not been found."
+actual_output=$("../build/solution/lab1" bin-search 2 100 101 | grep -v "Execution time")
 
 if [ "$expected_output" = "$actual_output" ]; then
     echo "Test passed: binary search works correctly."
